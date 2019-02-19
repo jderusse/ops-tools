4bc18eb44633529bcfa34c016d2be754dfff28ed
Jérémy Derussé <jeremy@derusse.com>
2019-02-19 11:59:38 +0100

    Move swap location

--- swap.sh
+++ swap.sh
@@ -2,8 +2,8 @@
 
 swapon -s|grep file && echo "Already swapped" && exit 0
 
-dd if=/dev/zero of=/swapfile bs=1G count=2
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+dd if=/dev/zero of=/var/swap.1 bs=1G count=2
+chmod 600 /var/swap.1
+mkswap /var/swap.1
+swapon /var/swap.1
+echo "/var/swap.1 swap swap defaults 0 0" >> /etc/fstab
